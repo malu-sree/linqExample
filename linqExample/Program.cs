4f3aacf6a9428aa069a3ab8e2751846a4f668ecf
malu-sree <sreelakshmipalangattu@gmail.com>
2025-03-07 16:19:43 +0530

    2

--- linqExample/Program.cs
+++ linqExample/Program.cs
@@ -1,52 +1,52 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-//namespace LinqExample
-//{
-//    class Product
-//    {
-//        public string Name;
-//        public string Category;
-//        public double Price;
+namespace LinqExample
+{
+    class Product
+    {
+        public string Name;
+        public string Category;
+        public double Price;
 
-//        public Product(string name, string category, double price)
-//        {
-//            Name = name;
-//            Category = category;
-//            Price = price;
+        public Product(string name, string category, double price)
+        {
+            Name = name;
+            Category = category;
+            Price = price;
 
-//        }
-//    }
-//    class Program2
-//    {
+        }
+    }
+    class Program2
+    {
 
-//        static void Main(string[] args)
-//        {
-//            List<Product> products = new List<Product>()
-//            {
-//                new Product("Laptop", "Electronics", 800),
-//                new Product("Smartphone", "Electronics", 600),
-//                new Product("Table", "Furniture", 150),
-//                new Product("Sofa", "Furniture", 500),
-//                new Product("Headphones", "Electronics", 50)
-//            };
-//            string selectedCategory = "Electronics";
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product("Laptop", "Electronics", 800),
+                new Product("Smartphone", "Electronics", 600),
+                new Product("Table", "Furniture", 150),
+                new Product("Sofa", "Furniture", 500),
+                new Product("Headphones", "Electronics", 50)
+            };
+            string selectedCategory = "Electronics";
 
-//            var filterdProducts = products.Where(p => p.Category == selectedCategory).ToList();
-//            foreach (var product in filterdProducts)
-//            {
-//                Console.WriteLine($"Product name :{product.Name}, Price:{product.Price} ");
-//            }
-//            double averagePrice = filterdProducts.Average(p => p.Price);
+            var filterdProducts = products.Where(p => p.Category == selectedCategory).ToList();
+            foreach (var product in filterdProducts)
+            {
+                Console.WriteLine($"Product name :{product.Name}, Price:{product.Price} ");
+            }
+            double averagePrice = filterdProducts.Average(p => p.Price);
 
-//            Console.WriteLine($"Average price of all products is {averagePrice}");
+            Console.WriteLine($"Average price of all products is {averagePrice}");
 
 
-//            Console.WriteLine("Press any key to exit...");
-//            Console.ReadKey();
-//        }
-//    }
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
 
-//}
+}
 
--- linqExample/Program2.cs
+++ linqExample/Program2.cs
@@ -1,53 +1,53 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace linqExample2
-//{
-//    class Product
-//    {
-//        public string Name;
-//        public string Category;
-//        public double Price;
-//        public Product(string name, string category, double price)
-//        {
-//            Name = name;
-//            Category = category;
-//            Price = price;
-//        }
-//    }
-//    class Program
-//    {
-//        public static void Main(string[] args)
-//        {
-//            List<Product> products = new List<Product>()
-//            {
-//                new Product("Laptop", "Electronics", 800),
-//                new Product("Smartphone", "Electronics", 600),
-//                new Product("Table", "Furniture", 150),
-//                new Product("Sofa", "Furniture", 500),
-//                new Product("Headphones", "Electronics", 50),
-//                new Product("Chair", "Furniture", 100),
-//                new Product("Refrigerator", "Appliances", 1200),
-//                new Product("Microwave", "Appliances", 300)
-//            };
+namespace linqExample2
+{
+    class Product
+    {
+        public string Name;
+        public string Category;
+        public double Price;
+        public Product(string name, string category, double price)
+        {
+            Name = name;
+            Category = category;
+            Price = price;
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product("Laptop", "Electronics", 800),
+                new Product("Smartphone", "Electronics", 600),
+                new Product("Table", "Furniture", 150),
+                new Product("Sofa", "Furniture", 500),
+                new Product("Headphones", "Electronics", 50),
+                new Product("Chair", "Furniture", 100),
+                new Product("Refrigerator", "Appliances", 1200),
+                new Product("Microwave", "Appliances", 300)
+            };
 
-//            var groupedProducts = from p in products
-//                                  group p by p.Category into g
-//                                  orderby g.Count() descending
-//                                  select new { Category = g.Key, Count = g.Count() };
-//            foreach (var group in groupedProducts)
-//            {
-//                Console.WriteLine($"Category: {group.Category}, ProductNumbers: {group.Count}");
-//            }
-//            Console.WriteLine("\nPress any key to exit...");
-//            Console.ReadKey();
+            var groupedProducts = from p in products
+                                  group p by p.Category into g
+                                  orderby g.Count() descending
+                                  select new { Category = g.Key, Count = g.Count() };
+            foreach (var group in groupedProducts)
+            {
+                Console.WriteLine($"Category: {group.Category}, ProductNumbers: {group.Count}");
+            }
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
 
 
 
-//        }
+        }
 
-//    }
-//}
+    }
+}
